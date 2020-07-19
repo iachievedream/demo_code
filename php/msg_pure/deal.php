c8e9de20b2b5fec0dd337d0d97c938b8a6f10e19
iachievedream <iachievedream@gmail.com>
2020-07-19 13:47:59 +0800

    update pure_php code and review this code

--- php/msg_pure/deal.php
+++ php/msg_pure/deal.php
@@ -1,33 +1,57 @@
-<?php
-require_once('db.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>deal</title>
+    <link href="css.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <?php
+    require_once('db.php');
 
-//使用$_POST接住表單傳送過來的資料，確認新增留言有點選，後續執行sql指令，並判斷是否新增成功
-$name = $_POST['name'];
-$msg = $_POST['msg'];
-if ($_POST['submit']) {
-    $sql = "insert into message_board(name,msg,update_time) values ('$name','$msg',now())";
-    if (mysqli_query($db, $sql)) {
-        echo "留言新增成功";
-    } else {
-        echo "留言新增失敗";
+    //使用$_POST接住表單傳送過來的資料，確認新增留言有點選，後續執行sql指令，並判斷是否新增成功
+    if ($_POST['add_submit']) {
+        $name = $_POST['name'];
+        $msg = $_POST['msg'];   
+        $sql = "insert into message_board(name,msg,update_time) values ('$name','$msg',now())";
+        if (mysqli_query($db, $sql)) {
+            echo "msg add successfully";
+        } else {
+            echo "msg add failed";
+        }
     }
-}
 
-//確認參數id是否有抓取，後續執行刪除sql指令，並判斷是否刪除成功
-$id = $_GET['id'];
-if ($_GET['id']) { //null or number
-    $delete = "delete from message_board where id=" . $id;
-    if (mysqli_query($db, $delete)) {
-        echo "刪除成功";
-    } else {
-        echo "刪除失敗";
+    //確認參數delete是否為1，後續抓取id以及執行刪除sql指令，並判斷是否刪除成功
+    if ($_GET['delete']) { //null or number
+        $id = $_GET['id'];
+        $delete = "delete from message_board where id=" . $id;
+        if (mysqli_query($db, $delete)) {
+            echo "delete successfully";
+        } else {
+            echo "delete failed";
+        }
     }
-}
-?>
-<br>
-<button>
-    <a href="index.php">留言版首頁</a>
-</button>
-<button>
-    <a href="add.html">新增留言</a>
-</button>
+
+    if ($_POST['update_submit']) {
+        $id = $_GET['id'];
+        $name = $_POST['update_name'];
+        $msg = $_POST['update_msg'];
+        $sql = "update message_board set name='".$name."',msg='".$msg."' where message_board.id=".$id;
+        // var_dump($sql);die;
+        if (mysqli_query($db, $sql)) {
+            echo "msg update successfully";
+        } else {
+            echo "msg update failed";
+        }
+    }
+    ?>
+    <br>
+    <button>
+        <a href="index.php">msg index</a>
+    </button>
+    <button>
+        <a href="add.html">add msg</a>
+    </button>
+</body>
+</html>
--- php/msg_pure/index.php
+++ php/msg_pure/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>留言板</title>
+    <title>msg</title>
     <link href="css.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
     <button>
-        <a href="add.html">新增留言</a>
+        <a href="add.html">add msg</a>
     </button>
     <br>
     <?php
@@ -16,17 +18,48 @@
     $sql = "select * from message_board order by id asc";//sql指令,order by id照id的順序排列，desc由大至小，asc由小至大
     $result = mysqli_query($db, $sql);//執行指令
     if (mysqli_num_rows($result) > 0) {//共取得幾筆記錄
+    ?>
+        <table border="0" width=400>
+            <tr align="center">
+                <td>id</td>
+                <td>name</td>
+                <td>msg</td>
+                <td>update time</td>
+                <td width=100>note</td>
+            </tr>
+        <?php
         while ($row = mysqli_fetch_assoc($result)) {//取一筆個別列出
-            echo "<br>" . $row["id"] . " -- " .$row["name"] . " -- " .$row["msg"] . " -- " . $row["update_time"];
-             ?>
-             <button>
-                 <a href="deal.php?id=<?php echo $row["id"]; ?>">刪除</a>
-            </button>
-             <?php
+        ?>
+        <tr>
+            <td width=5%>
+                <?php echo $row["id"]; ?>
+            </td>
+            <td width=15%>
+                <?php echo $row["name"]; ?>
+            </td>
+            <td width=20%>
+                <?php echo $row["msg"]; ?>
+            </td>
+            <td width=130>
+                <?php echo $row["update_time"]; ?>
+            </td>
+            <td width=100>
+                <button>
+                    <a href="updata.php?id=<?php echo $row["id"]; ?>">edit</a>
+                </button>
+                &nbsp
+                <button>
+                    <a href="deal.php?delete=1&id=<?php echo $row["id"]; ?>">delete</a>
+                </button>
+            </td>
+        </tr>
+    <?php
         }
     } else {
-        echo "沒有留言資訊";
+        echo "no msg massage.";
     }
-?>
+    ?>
+    </table>
 </body>
+
 </html>
