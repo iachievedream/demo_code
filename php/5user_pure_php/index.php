459d4af04ce05023a5899b3adbb604b994f61311
iachievedream <iachievedream@gmail.com>
2020-08-24 01:14:35 +0800

    try class and new

--- php/5user_pure_php/index.php
+++ php/5user_pure_php/index.php
@@ -1,18 +1,22 @@
 <?php
 class index {
-    public function index() {
+    public function index1() {
         if($_SESSION['login_session'] = null && $_SESSION['username'] = null){
             echo "非法登入";
             // sleep(3);
             // header ("Location:login.php");
         } else {
-            echo "登入模式";
+            echo "登入模式12";
         }
     }
 
+    public function index2() {
+            echo "非法登入";
+    }
 
 }
 
-echo "abc";
+// echo "abc";
 $ab = new index;
-echo $ab;
+$abc = $ab->index2;
+echo $abc;
